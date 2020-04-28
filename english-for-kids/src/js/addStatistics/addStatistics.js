b4e7360438bf07879ee1a8f263e1e2d25511a3dc
Alexey Radzuk <radyukag@yandex.ru>
2020-04-28 22:29:07 +0200

    feat: add basic markup and styles

--- english-for-kids/src/js/addStatistics/addStatistics.js
+++
@@ -1,8 +0,0 @@
-export default function addStatistics (list, statistics) { 
-    const obj = list[list.length - 1];
-    statistics.push(obj);
-    list.pop();
-    if (list.length > 0) {
-        addStatistics(list, statistics);
-    } 
-}
