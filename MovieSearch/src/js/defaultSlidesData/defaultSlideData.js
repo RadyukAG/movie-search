9c7bcd9efd512fd18a6f3e97a97d4ab5831b5540
Alexey Radzuk <radyukag@yandex.ru>
2020-05-03 20:07:00 +0200

    feat: add slider

---
+++ MovieSearch/src/js/defaultSlidesData/defaultSlideData.js
@@ -0,0 +1,14 @@
+const defaultSlidesArray = [
+{Title: "Casino Royale", Year: "2006", imdbID: "tt0381061", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMDI5ZWJhOWItYTlhOC00YWNhLTlkNzctNDU5YTI1M2E1MWZhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg", rate: "8.0"},
+{Title: "Casino", Year: "1995", imdbID: "tt0112641", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMTcxOWYzNDYtYmM4YS00N2NkLTk0NTAtNjg1ODgwZjAxYzI3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg", rate: "8.2"},
+{Title: "Casino Royale", Year: "1967", imdbID: "tt0061452", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMTM2NzQ4NDE2M15BMl5BanBnXkFtZTcwNDM0NzcwNA@@._V1_SX300.jpg", rate: "5.2"},
+{Title: "Casino Jack", Year: "2010", imdbID: "tt1194417", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMTM2NzQ4NDE2M15BMl5BanBnXkFtZTcwNDM0NzcwNA@@._V1_SX300.jpg", rate: "6.2"},
+{Title: "Casino Jack and the United States of Money", Year: "2010", imdbID: "tt1540814", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMjA3MTc2MzM4NV5BMl5BanBnXkFtZTcwMzE5MjYxMw@@._V1_SX300.jpg", rate: "7.1"},
+{Title: "The Last Casino", Year: "2004", imdbID: "tt0419909", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BZmJkMTA5YTMtZjNkZS00ODgwLTg4MDQtMWFmMDMwYWZhNTBhXkEyXkFqcGdeQXVyNTY0OTIxNTg@._V1_SX300.jpg", rate: "7.2"},
+{Title: "The Casino Job", Year: "2009", imdbID: "tt0831279", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMTMzNjQwNTI2N15BMl5BanBnXkFtZTcwNDE5MzAyMg@@._V1_SX300.jpg", rate: "3.5"},
+{Title: "Cleopatra Jones and the Casino of Gold", Year: "1975", imdbID: "tt0072791", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMjUyNTUxNjEtOTMzNy00MmZiLTk2NDctOTIwZjNhZmY2YzY5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_SX300.jpg", rate: "5.7"},
+{Title: "Gran Casino", Year: "1947", imdbID: "tt0039434", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMTc0NDExMDUzN15BMl5BanBnXkFtZTcwNDE0OTM1MQ@@._V1_SX300.jpg", rate: "6.0"},
+{Title: "The Haunted Casino", Year: "2007", imdbID: "tt0864331", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BMTk2ODk4MzUyMF5BMl5BanBnXkFtZTcwMjI3MjE2MQ@@._V1_SX300.jpg", rate: "4.0"},
+];
+
+export default defaultSlidesArray;
--- MovieSearch/src/js/index.js
+++ MovieSearch/src/js/index.js
@@ -1,13 +1,63 @@
-import Swiper from '../swiper/js/swiper';
+import Swiper from 'swiper/js/swiper.min';
 import '../swiper/css/swiper.min.css';
-import '../swiper/js/swiper.min';
+import defaultSlidesArray from "./defaultSlidesData/defaultSlideData";
+
 import '../css/normalize.css';
 import '../css/style.css';
 
 const mySwiper = new Swiper('.swiper-container', {
-    width: '100%',
-    
+    slidesPerView: 3,
+    freeMode: true,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+   breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    850: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    1100: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+    }
+  }
 });
+
+function createSlide (obj) {
+    const slideContent = document.createElement('div');
+    slideContent.insertAdjacentHTML('beforeend', `<a class="swiper-slide__title" href="https://www.imdb.com/title/${obj.imdbID}/videogallery">${obj.Title}</a>`);
+    slideContent.insertAdjacentHTML('beforeend', `<img class="swiper-slide__poster" src="${obj.Poster}">`);
+    slideContent.insertAdjacentHTML('beforeend', `<p class="swiper-slide__year">${obj.Year}</p>`);
+    slideContent.insertAdjacentHTML('beforeend', `<p class="swiper-slide__rate">${obj.rate}</p>`);
+    const slide = document.createElement('div');
+    slide.classList.add('swiper-slide');
+    slide.appendChild(slideContent);
+    mySwiper.appendSlide(slide);
+    mySwiper.update();
+}
+
+function addSlides(array) {
+    mySwiper.removeAllSlides();
+    array.forEach(el => {
+        createSlide(el);
+    });
+}
+
+addSlides(defaultSlidesArray);
 
 const APIurl = 'http://www.omdbapi.com/?';
 const APIkey = '&apikey=bd3c609';
@@ -18,28 +68,20 @@ const elements = {
     keyboardIcon: document.querySelector('.keyboard-icon'),
 }
 
-function createSlides(array) {
-    const slides = document.getElementsByClassName('swiper-slide');
-    Array.prototype.forEach.call(slides, (el, i) => {
-        const title = el.getElementsByClassName('swiper-slide__title');
-        title.value = array[i].Title;
-        title.href = `https://www.imdb.com/title/${array[i].imdbID}/videogallery`;
-        el.getElementsByClassName('swiper-slide__poster').src = `url(${array[i].Poster})`;
-    });
-}
 
-function getRateFromOMDB(array) {
-    const moviesData = array.map(el => {
-        const rateRequest = `${APIurl}i=${el.imdbID}${APIkey}`;
-        fetch (rateRequest)
+
+function getRateFromOMDB (array) {
+    function requestRate (elem) {
+        const rateRequest = `${APIurl}i=${elem.imdbID}${APIkey}`;
+        return fetch (rateRequest)
             .then(response => response.json())
             .then(detailedMovieData => detailedMovieData.Ratings[0].Value)
-            .then(function addRate(rateString) {
-                el.rate = rateString.slice(0, -3);
+            .then(async function addRate(rateString) {
+                elem.rate = rateString.slice(0, -3);
+                return elem;
             });
-            return el
-    });
-    return moviesData;
+    }
+    return Promise.all(array.map(el => requestRate(el)));
 }
 
 function getSearchResultsFromOMDB (data) {
@@ -47,14 +89,15 @@ function getSearchResultsFromOMDB (data) {
     fetch (request)
         .then(response => response.json())
         .then(object => object.Search)
-        .then(array => getRateFromOMDB(array));
+        .then(array => getRateFromOMDB(array))
+        .then(array => addSlides(array))
+        .catch(error => console.log(error));
 }
 
-function formSubmitHandler(e) {
+async function formSubmitHandler(e) {
     e.preventDefault();
     const searchRequest = elements.input.value;
-    const moviesData = getSearchResultsFromOMDB(searchRequest);
-    createSlides(moviesData);
+    getSearchResultsFromOMDB(searchRequest);
 }
 
 elements.searchForm.addEventListener('submit', formSubmitHandler);
